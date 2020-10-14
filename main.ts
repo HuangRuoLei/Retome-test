e8da38fcea7f3c664f91ecc8b7d2ab0a7b3a3cd3
HuangRuoLei <1694001042@qq.com>
2020-10-14 11:24:38 +0800

    A

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color="#008FFF" weight=2 0 icon="\uf11c" block="呼噜猫遥控器通信确认"
+//% color="#000000" weight=2 0 icon="\uf11b" block="呼噜猫遥控器通信确认"
 namespace TuoYuCar_connection {
     /**
      * 调用此来建立与遥控器的通信,通信建立成功则返回55
@@ -7,7 +7,7 @@ namespace TuoYuCar_connection {
     //% blockId=TuoYuCar_connection_con block="建立与遥控器的通信"
     //% weight=100
     //% blockGap=10
-    //% color="#FFAAFF"
+    //% color="#000000"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function con(): void {
         let length;
@@ -24,7 +24,7 @@ namespace TuoYuCar_connection {
     }
 }
 
-//% color="#008FFF" weight=2 0 icon="\uf11c" block="呼噜猫遥控器传感器类"
+//% color="#000000" weight=2 0 icon="\uf11b" block="呼噜猫遥控器传感器类"
 namespace TuoYuCar {
 
     export enum ultrasonicState{
@@ -66,7 +66,7 @@ namespace TuoYuCar {
     //% blockId=TuoYuCar_Chao_Sheng_Bo block="超声波测距系统|%index"
     //% weight=110
     //% blockGap=10
-    //% color="#FFAAFF"
+    //% color="#000000"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Chao_Sheng_Bo(index: ultrasonicState):void {
         basic.pause(10);
@@ -83,7 +83,7 @@ namespace TuoYuCar {
     //% blockId=TuoYuCar_Read_Chao_Sheng_Bo block="读取超声波测到的距离(cm)"
     //% weight=109
     //% blockGap=10
-    //% color="#FFAAFF"
+    //% color="#000000"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Read_Chao_Sheng_Bo(): number {
         let length;
@@ -98,7 +98,7 @@ namespace TuoYuCar {
     //% blockId=TuoYuCar_Bodycheck block="当人体传感器检测到人体或者活物时"
     //% weight=98
     //% blockGap=10
-    //% color="#FFAAFF"
+    //% color="#000000"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Bodycheck():boolean {
         let temp: boolean = false;
@@ -118,7 +118,7 @@ namespace TuoYuCar {
     //% blockId=TuoYuCar_Rain block="当水滴传感器检测到水滴时"
     //% weight=97
     //% blockGap=10
-    //% color="#FFAAFF"
+    //% color="#000000"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function Rain():boolean {
         let temp: boolean = false;
